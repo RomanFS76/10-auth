1b24e6d453ffe7ce9cc78ede78a0f95c32af3ec7
RomanFS76 <korvy85@gmail.com>
2026-02-13 18:01:00 +0300

    feat(profile): implement initial ProfilePage UI

---
+++ app/(private routes)/profile/page.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import css from './ProfilePage.module.css';
+
+
+const page = () => {
+  return (
+    <main className={css.mainContent}>
+      <div className={css.profileCard}>
+        <div className={css.header}>
+          <h1 className={css.formTitle}>Profile Page</h1>
+          <Link href="/profile/edit" className={css.editProfileButton}>
+            Edit Profile
+          </Link>
+        </div>
+        <div className={css.avatarWrapper}>
+          <Image
+            src="https://images.epochtimes.ru/uploads/06/cinema/169_05_06_10_Alise_2.jpg"
+            alt="User Avatar"
+            width={120}
+            height={120}
+            className={css.avatar}
+          />
+        </div>
+        <div className={css.profileInfo}>
+          <p>Username: your_username</p>
+          <p>Email: your_email@example.com</p>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default page;
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  images: {
+	  remotePatterns: [
+	      { protocol: 'https', hostname: 'images.epochtimes.ru' }
+	    ]
+	}
 };
 
 export default nextConfig;
