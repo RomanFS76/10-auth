83073f156ba53e28b5d0d31db9127afee53555c3
RomanFS76 <korvy85@gmail.com>
2026-02-14 09:33:07 +0300

    feat(auth): implement initial SignUpPage form and styles

--- app/(auth routes)/sign-in/page.tsx
+++ app/(auth routes)/sign-in/page.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import css from './SignInPage.module.css';
 
 const page = () => {
   return (
-    <div>4444</div>
-  )
-}
+    <main className={css.mainContent}>
+      <form className={css.form}>
+        <h1 className={css.formTitle}>Sign in</h1>
 
-export default page
+        <div className={css.formGroup}>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            type="email"
+            name="email"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className={css.input}
+            required
+          />
+        </div>
+
+        <div className={css.actions}>
+          <button type="submit" className={css.submitButton}>
+            Log in
+          </button>
+        </div>
+
+        <p className={css.error}>error</p>
+      </form>
+    </main>
+  );
+};
+
+export default page;
